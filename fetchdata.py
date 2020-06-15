57b6e47525ace8722a31edf0c450b50dcc9c004e
olebeck <psolebeck@gmail.com>
2020-06-15 18:37:27 +0200

    inital commit

---
+++ fetchdata.py
@@ -0,0 +1,24 @@
+import requests
+from datetime import datetime
+url = "https://discord.com/api/invite/2ccAkg3?with_counts=true"
+current = requests.get(url).json()
+
+count = current["approximate_member_count"]
+online = current["approximate_presence_count"]
+time = datetime.now()
+
+out = [count,online,url,time]
+f = None
+try:
+    f = open("data.csv","a")
+except FileNotFoundError:
+    f = open("data.csv","xa")
+
+i = 0
+for item in out:
+    if i > 0:
+        f.write(",")
+    f.write(str(item))
+    i = i + 1
+f.write("\n")
+f.close()
